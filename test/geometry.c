71e56390ad04e1937c30b412c39c8e46e6f6814b
zeffur <zeffurs@gmail.com>
2019-04-28 16:42:49 +0700

    Geometry.c tests are added

--- test/geometry.c
+++ test/geometry.c
@@ -1,17 +1,100 @@
-#include <sum.h>
+#include "../src/geometry.h"
+#include "../thirdparty/ctest.h"
 
-#include <ctest.h>
-
-CTEST(geometry, S_Circle)
+CTEST(circle, S_Circle)
 {
     // Given
-    const int S = 0;
-    Figure circle = {CIRCLE, {0 0 12}, 3};
+    double S = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 3};
 
     // When
-    S_Circle(&S, circle);
+    S_Circle(&S, &circle);
 
     // Then
-    const int exp = 452.389;
+    const double exp = 452.389324;
     ASSERT_DBL_NEAR(S, exp);
+}
+
+CTEST(circle, S_Circle1)
+{
+    // Given
+    double S = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 3};
+
+    // When
+    S_Circle(NULL, &circle);
+
+    // Then
+    const double exp = 0.0;
+    ASSERT_DBL_NEAR(S, exp);
+}
+
+CTEST(circle, S_Circle2)
+{
+    // Given
+    double S = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 3};
+
+    // When
+    S_Circle(NULL, NULL);
+
+    // Then
+    const double exp = 0.0;
+    ASSERT_DBL_NEAR(S, exp);
+}
+
+CTEST(circle, S_Circle3)
+{
+    // Given
+    double S = 0.0;
+    Figure circle = {CIRCLE, {0, 0}, 3};
+
+    // When
+    S_Circle(&S, &circle);
+
+    // Then
+    const double exp = 0.0;
+    ASSERT_DBL_NEAR(S, exp);
+}
+/*----------------------------------------------------------------------------*/
+CTEST(circle, P_Circle)
+{
+    // Given
+    double P = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 3};
+
+    // When
+    P_Circle(&P, &circle);
+
+    // Then
+    const double exp = 75.398224;
+    ASSERT_DBL_NEAR(P, exp);
+}
+
+CTEST(circle, P_Circle1)
+{
+    // Given
+    double P = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 3};
+
+    // When
+    P_Circle(NULL, &circle);
+
+    // Then
+    const double exp = 0.0;
+    ASSERT_DBL_NEAR(P, exp);
+}
+
+CTEST(circle, P_Circle2)
+{
+    // Given
+    double P = 0.0;
+    Figure circle = {CIRCLE, {0, 0, 12}, 0};
+
+    // When
+    P_Circle(&P, &circle);
+
+    // Then
+    const double exp = 0.0;
+    ASSERT_DBL_NEAR(P, exp);
 }
